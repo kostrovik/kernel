40ac22ce4abac27276555d123d1bcfad3b34a805
kostrovik <XFP-Xz4-8rF-sLd>
2018-12-17 17:34:48 +0300

    рефакторинг

--- src/main/java/com/github/kostrovik/kernel/views/DropDownDialog.java
+++ src/main/java/com/github/kostrovik/kernel/views/DropDownDialog.java
@@ -1,13 +1,14 @@
 package com.github.kostrovik.kernel.views;
 
 import com.github.kostrovik.kernel.dictionaries.SortDirection;
+import com.github.kostrovik.kernel.graphics.builders.ButtonBuilder;
 import com.github.kostrovik.kernel.graphics.common.icons.SolidIcons;
 import com.github.kostrovik.kernel.graphics.controls.base.InfinityTable;
 import com.github.kostrovik.kernel.graphics.controls.base.columns.PagedColumn;
 import com.github.kostrovik.kernel.interfaces.controls.ListFilterAndSorterInterface;
 import com.github.kostrovik.kernel.interfaces.controls.PaginationServiceInterface;
-import com.github.kostrovik.kernel.models.EmptyListFilter;
 import com.github.kostrovik.kernel.models.AbstractPopupWindow;
+import com.github.kostrovik.kernel.models.EmptyListFilter;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
@@ -44,10 +45,12 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
     private InfinityTable<T> selectedItemsTable;
 
     private ObservableList<T> selectedItems;
+    private ButtonBuilder buttonBuilder;
 
     public DropDownDialog(Pane parent) {
         super(parent);
         this.selectedItems = FXCollections.observableArrayList();
+        this.buttonBuilder = new ButtonBuilder();
     }
 
     @Override
@@ -75,7 +78,7 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
         createTable();
         createSelectedItemsTable();
 
-        Button button = facade.createButton("", SolidIcons.ARROW_ALT_RIGHT_LIGHT, SolidIcons.ARROW_ALT_RIGHT_SOLID, true);
+        Button button = buttonBuilder.createButton(SolidIcons.ARROW_ALT_RIGHT_LIGHT, SolidIcons.ARROW_ALT_RIGHT_SOLID, "", true);
 
         button.setOnAction(event -> selectedItems.addAll(table.getSelectedItems().stream().filter(item -> !selectedItems.contains(item)).collect(Collectors.toList())));
 
@@ -84,7 +87,10 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
         button.setPrefHeight(44);
         button.setMinWidth(0);
 
-        GridPane formLayout = facade.createTableFormLayout();
+        GridPane formLayout = new GridPane();
+        formLayout.setHgap(10);
+        formLayout.setVgap(10);
+        formLayout.setPadding(new Insets(10, 0, 0, 0));
 
         table.prefWidthProperty().bind(view.widthProperty().divide(2).subtract(button.widthProperty()));
         selectedItemsTable.prefWidthProperty().bind(view.widthProperty().divide(2).subtract(button.widthProperty()));
@@ -105,12 +111,12 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
 
     @Override
     protected Region getWindowButtons() {
-        Button clearButton = facade.createButton("Очистить");
+        Button clearButton = buttonBuilder.createButton("Очистить");
         Separator separator = new Separator();
         separator.setOrientation(Orientation.VERTICAL);
 
-        Button saveButton = facade.createButton("Сохранить");
-        Button cancelButton = facade.createButton("Отмена");
+        Button saveButton = buttonBuilder.createButton("Сохранить");
+        Button cancelButton = buttonBuilder.createButton("Отмена");
 
         clearButton.setOnAction(event -> selectedItems.clear());
 
@@ -154,7 +160,8 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
 
         table = new InfinityTable<>(paginationService, defaultFilter);
 
-        PagedColumn<T, String> name = facade.createTableColumn("Название", listLabelCallback);
+        PagedColumn<T, String> name = new PagedColumn<>("Название");
+        name.setCellValueFactory(listLabelCallback);
         table.getColumns().addAll(name);
         table.setMultiselection(true);
     }
@@ -162,11 +169,12 @@ public class DropDownDialog<T> extends AbstractPopupWindow {
     private void createSelectedItemsTable() {
         selectedItemsTable = new InfinityTable<>(selectedItems);
 
-        PagedColumn<T, String> name = facade.createTableColumn("Название", listLabelCallback);
+        PagedColumn<T, String> name = new PagedColumn<>("Название");
+        name.setCellValueFactory(listLabelCallback);
 
-        PagedColumn<T, Button> action = facade.createTableColumn("Действие");
+        PagedColumn<T, Button> action = new PagedColumn<>("Действие");
         Callback<T, Button> actionValueCallback = param -> {
-            Button actionButton = facade.createButton("Удалить");
+            Button actionButton = buttonBuilder.createButton("Удалить");
             actionButton.setOnAction(event -> removeItem(param));
 
             actionButton.setMinWidth(73);
